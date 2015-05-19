34d63a42c2f3cd21860be95f3a0af33d2da73083
David Dawson <daws23@gmail.com>
2015-05-19 15:04:20 -0700

    implement Aws::EC2::Instance#running? bump to version 2.0.1

--- lib/aws-sdk/enhanced/ec2.rb
+++ lib/aws-sdk/enhanced/ec2.rb
@@ -3,6 +3,12 @@ module EC2
 
   class Instance
     include Aws::Enhanced::TagHashable
+
+    def running?
+      state.name == 'running'
+    rescue Aws::EC2::Errors::InvalidInstanceIDNotFound
+      false
+    end
   end
 
 end
